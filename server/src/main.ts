2d7501be7f67d3286860ed8b3aa476b272018987
Anshul Garwal <80778090+anshul45@users.noreply.github.com>
2025-01-04 19:30:49 +0530

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-		origin: "http://localhost:5173",
+		origin: "https://twitter-analyzer-1.onrender.com",
 		methods: "GET,POST,PATCH",
 	});
   await app.listen(process.env.PORT ?? 3000);
