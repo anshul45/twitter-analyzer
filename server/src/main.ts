01d06a3a81c775f7bd04d433b3c33965fbb05314
Abhilasha <singhabhilasha4042@gmail.com>
2025-01-04 19:17:01 +0530

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-		origin: "http://localhost:5173",
-		methods: "GET,POST,PATCH",
-	});
+    origin: '*',
+    methods: 'GET,POST,PATCH',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
