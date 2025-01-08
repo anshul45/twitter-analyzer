07f0042c9b8dd8c2eee2fcb07a2f36f455eb517f
anshul45 <anshulgarwal025@gmail.com>
2025-01-08 23:40:11 +0530

    updated backend and connect to frontend

--- server/src/cron/cron.service.ts
+++ server/src/cron/cron.service.ts
@@ -1,15 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { TwitterService } from 'src/twitter/twitter.service';
 
 @Injectable()
 export class CronService {
-  @Cron('0 5 * * *')
-  handleMorningJob() {
-    console.log('Cron job will run at 5:00 AM every day');
+  // Injecting the TwitterService and defining the list of usernames
+  private readonly usernames = [
+    'pakpakchicken',
+    'fundstrat',
+    'BourbonCap',
+    'ripster47',
+    'Micro2Macr0',
+    'LogicalThesis',
+    'RichardMoglen',
+    'Couch_Investor',
+    'StockMarketNerd',
+    'MCins_',
+    'unusual_whales',
+  ];
+
+  constructor(private readonly twitterService: TwitterService) {}
+
+  // Cron job to run at Pacific time 5:00 AM every day
+  @Cron('0 1 * * *')
+  async handleMorningJob() {
+    try {
+      await Promise.all(
+        this.usernames.map(async (user) => {
+          try {
+            await this.twitterService.savetoDB(user);
+            console.log(`Successfully added user: ${user}`);
+          } catch (error) {
+            console.error(`Error adding user ${user}:`, error.message);
+          }
+        })
+      );
+    } catch (error) {
+      console.error('Error in morning cron job:', error.message);
+    }
   }
 
-  @Cron('0 16 * * *')
-  handleFourPM() {
-    console.log('Cron job will run at 4:00 PM every day');
+  // Cron job to run at Pacific time 4:00 PM every day
+  @Cron('0 0 * * *')
+  async handleFourPM() {
+    try {
+      await Promise.all(
+        this.usernames.map(async (user) => {
+          try {
+            await this.twitterService.savetoDB(user);
+            console.log(`Successfully added user: ${user}`);
+          } catch (error) {
+            console.error(`Error adding user ${user}:`, error.message);
+          }
+        })
+      );
+    } catch (error) {
+      console.error('Error in morning cron job:', error.message);
+    }
+  
   }
 }
